68d90fe582fb557d5009a30f62f73a8f3c467d0b
halid vejzovic <halidvejzovic@outlook.com>
2026-09-16 20:27:43 +0200

    9:v

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 
-let namn = prompt("Vad heter du?");
-alert("hej " + namn + "! Välkommen till vår webbplats");
+///let namn = prompt("Vad heter du?");
+/*alert("hej " + namn + "! Välkommen till vår webbplats");
+
+/*let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum += i;
+}
+
+alert("Summan av talen från 0 till 9 år:" + sum);*/
+
+let pRef = document.getElementById("text");
+pRef.innerHTML = "Hej! Välkommen till vår webbplats.";
+
+console.log(pRef);
