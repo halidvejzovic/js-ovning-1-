416bd59c62dbb718be88d3d20fc6a6559ee344e5
halid vejzovic <halidvejzovic@outlook.com>
2026-09-16 21:13:10 +0200

    12:v

--- main.js
+++ main.js
@@ -9,19 +9,28 @@ for(let i = 0; i < 10; i++){
 
 alert("Summan av talen från 0 till 9 år:" + sum);*/
 
-let pRef;
+let pRef, buttonRef;
 
 function init() {
-   let pRef = document.getElementById("text"); 
+    pRef = document.getElementById("text"); 
+    buttonRef = document.getElementById("myButton");
     console.log(pRef);
-
+    console.log(buttonRef);
+    
     pRef.innerHTML = "Hej! Välkommen till vår webbplats.";
 
     pRef.style.color = "blue";
+
+    buttonRef.addEventListener("click", changeText);
+
 }
 
 window.onload = init;
 
+function changeText() {
+    pRef.innerHTML = "Du klickade på knappen";
+    pRef.style.color = "red";
+}
 
 
 
