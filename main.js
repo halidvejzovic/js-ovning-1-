def61b3bd167318241df7214d6cc0b76dd893c0f
halid vejzovic <halidvejzovic@outlook.com>
2026-09-19 20:30:39 +0200

    32:v

--- main.js
+++ main.js
@@ -96,15 +96,18 @@ for (let i = 1; i<=7; i++ ) { //defination av i samt start, vilkor, ökningav i
 
 
 for (let i = 1; i<=100; i++) {
+
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(FizzBuzz);
+    }
+
     if(i % 3 === 0) {
         console.log("Fizz");
     }
-    if (i % 3 === 0) {
+    if (i % 5 === 0) {
         console.log("Buzz");
     }
-    if (i % 3 % 5 === 0) {
-        console.log("FizzBuzz");
-    }
+    
     else{
         console.log(i);
         
