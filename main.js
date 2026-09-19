838ab4054d92bc5eefa5dee80bef847475dfc83e
halid vejzovic <halidvejzovic@outlook.com>
2026-09-19 20:23:14 +0200

    31:v

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ for (let i = 1; i<=100; i++) {
     if (i % 3 === 0) {
         console.log("Buzz");
     }
-    if (i % 3, 5 === 0) {
+    if (i % 3 % 5 === 0) {
         console.log("FizzBuzz");
     }
     else{
