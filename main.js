b7eaf6f17e7d98d54e6b08ef4cb18e36af7c55eb
halid vejzovic <halidvejzovic@outlook.com>
2026-09-20 16:37:33 +0200

    36:v

--- main.js
+++ main.js
@@ -121,7 +121,16 @@ for (let i = 0; i < 8; i++) {
 console.log(rad);  */
 
 
-for ()
+for (let i = 0; i < 8; ++i) {
+
+    let rad = ""
+    for (let i = 0; i < 8; i++) {
+        rad +="* ";
+    }
+    console.log(rad);
+    
+}
+
 
 
 
